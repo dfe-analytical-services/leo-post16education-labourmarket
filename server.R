0dc2366013e851455dcc03609563cea28793080a
Lewis Spencer <lewis.spencer@education.gov.uk>
2022-05-11 17:32:38 +0100

    Add mean hline to example plot

--- server.R
+++ server.R
@@ -12,6 +12,7 @@ library(shinyjs)
 library(ggplot2)
 library(plotly)
 library(govstyle)
+library(dplyr)
 
 # Define server logic 
 server <- function(input, output, session) {
@@ -26,17 +27,34 @@ server <- function(input, output, session) {
   })
   
   output$plot <- renderPlotly({
+    mpg_mean <- mean(mtcars$mpg)
     ggplotly(
       ggplot(
         data = mtcars, 
         mapping = aes(
           x = wt, 
           y = mpg,
-          col = factor(cyl)
+          col = factor(cyl),
+          text = row.names(mtcars)
         )
-      ) + 
-      geom_point() +
-      govstyle::theme_gov()
-    )
+      ) 
+      + geom_point()
+      + geom_hline(
+        aes(
+          yintercept = mpg_mean,
+        ),
+        linetype = "dashed",
+        colour = 'black',
+        size=0.4
+      )
+      + annotate(
+        geom="text", 
+        label="Mean", 
+        x=1, 
+        y=mpg_mean + 1, 
+        vjust=-1
+      )
+      + govstyle::theme_gov()
+    ) 
   })
 }
