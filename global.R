b65309f428a9482c533623b856f6cf3b3db86879
Lewis Spencer <lewis.spencer@education.gov.uk>
2022-05-12 18:08:18 +0100

    Read dummy map data and region shape file and move libraries into global scope

--- global.R
+++ global.R
@@ -1,2 +1,41 @@
+# shiny app development and appearance
+library(shiny)
+library(shinyGovstyle)
+
+# data import and manipulation
+library(dplyr)
+library(rjson)
+library(readr)
+
+# visuals
+library(plotly)
+library(ggplot2)
+
+# geo-spatial data and mapping
+library(sf)
+library(leaflet)
+library(leaflet.extras)
+library(viridis)
+
 source("helpers/import_data.R", encoding = "UTF-8")
 source("helpers/components.R", encoding = "UTF-8")
+
+
+# Reading shape files - https://cengel.github.io/R-spatial/intro.html#loading-shape-files-into-r
+
+earnings_by_region = readr::read_csv(
+  file = "data/dummy_earnings_data.csv",
+  show_col_types = FALSE
+)
+
+regions <- sf::read_sf(
+  dsn = "shapes",
+  layer = "Regions_December_2018_EN_BFC"
+) %>%
+sf::st_transform('+proj=longlat +datum=WGS84')
+
+map_data <- regions %>%
+  left_join(
+    earnings_by_region,
+    by = c("rgn18cd" = "RegionID")
+  )
