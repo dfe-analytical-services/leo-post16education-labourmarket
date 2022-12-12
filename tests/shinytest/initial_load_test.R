d26d8319233ee3fb5d210a22c10451c93b2f0a16
Emily Yau <emily.yau@education.gov.uk>
2022-12-12 14:08:49 +0000

    adding inputs to snapshot initial load testing

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -15,3 +15,26 @@ app$snapshot()
 app$setInputs(navlistPanel = "panel9")
 
 app$snapshot()
+
+
+
+app$setInputs(input_id="earn_select1")
+app$snapshot()
+
+app$setInputs(input_id="earn_subcat")
+app$snapshot()
+
+app$setInputs(input_id="earn_picker")
+app$snapshot()
+
+app$setInputs(input_id="comparisoncheck")
+app$snapshot()
+
+app$setInputs(input_id="activity_select1")
+app$snapshot()
+
+app$setInputs(input_id="sub_group_picker")
+app$snapshot()
+
+app$setInputs(input_id="picker1")
+app$snapshot()
